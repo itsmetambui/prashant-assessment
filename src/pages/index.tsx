619e91fb94f7182d827adc4dd32a112a952620f9
tambEW <itsmetambui@gmail.com>
2021-12-13 21:44:55 +0700

    feat: add normal pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import makeData from "../app/makeData";
 import Table from "../components/Table";
 
 const IndexPage: NextPage = () => {
-  const data = React.useMemo(() => makeData(100), []);
+  const serverData = React.useMemo(() => makeData(200), []);
 
   const columns = React.useMemo(
     () => [
@@ -28,7 +28,49 @@ const IndexPage: NextPage = () => {
     []
   );
 
-  return <Table data={data} columns={columns} />;
+  // We'll start our table without any data
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [pageCount, setPageCount] = React.useState(0);
+  const fetchIdRef = React.useRef(0);
+
+  const fetchData = React.useCallback(({ pageSize, pageIndex }) => {
+    // This will get called when the table needs new data
+    // You could fetch your data from literally anywhere,
+    // even a server. But for this example, we'll just fake it.
+
+    // Give this fetch an ID
+    const fetchId = ++fetchIdRef.current;
+
+    // Set the loading state
+    setLoading(true);
+
+    // We'll even set a delay to simulate a server here
+    setTimeout(() => {
+      // Only update the data if this is the latest fetch
+      if (fetchId === fetchIdRef.current) {
+        const startRow = pageSize * pageIndex;
+        const endRow = startRow + pageSize;
+        setData(serverData.slice(startRow, endRow));
+
+        // Your server could send back total page count.
+        // For now we'll just fake it, too
+        setPageCount(Math.ceil(serverData.length / pageSize));
+
+        setLoading(false);
+      }
+    }, 1000);
+  }, []);
+
+  return (
+    <Table
+      columns={columns}
+      data={data}
+      fetchData={fetchData}
+      loading={loading}
+      pageCount={pageCount}
+    />
+  );
 };
 
 export default IndexPage;
