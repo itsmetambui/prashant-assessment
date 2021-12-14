751a68ea044826e7969147b4194589b30b711c49
tambEW <itsmetambui@gmail.com>
2021-12-14 12:42:15 +0700

    chore: remove logs

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -3,15 +3,12 @@ import mock from "./adapter";
 
 const totalRecords = 1000;
 const data = makeData(totalRecords);
-console.log(data);
 
 mock.onGet(new RegExp(`/students/*`)).reply(function (config) {
   const urlSearchParams = new URLSearchParams(config.url.split("?")[1]);
   const { searchTerm, limit, skip } = Object.fromEntries(
     urlSearchParams.entries()
   );
-  console.log(searchTerm, limit, skip);
-
   return [
     200,
     {
